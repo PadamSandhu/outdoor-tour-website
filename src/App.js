9575eede4ae43cfd5431d75efae45d795345279d
Padam <padam@Padams-iMac.local>
2019-03-22 16:23:53 -0400

    Updated Css Architecture

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import {Header} from "./components/header";
-import './styles/index.scss';
+import React, { Component } from "react";
+import { Header } from "./components/header";
+import "./styles/index.scss";
 
 class App extends Component {
   render() {
+    console.log("this Application uses BEM css Architecture");
     return (
       <div className="App">
-          <Header/>
+        <Header />
       </div>
     );
   }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,16 +6,16 @@ import Button from "../common/button";
 export const Header = () => {
   return (
     <div className="header">
-      <div className="logo-box">
-        <img src={whiteLogo} alt="Logo" className="logo" />
+      <div className="header__logo-box">
+        <img src={whiteLogo} alt="Logo" className="header__logo" />
       </div>
 
-      <div className="text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main"> Outdoors </span>
-          <span className="heading-primary-sub"> is where life happens </span>
+          <span className="heading-primary--main"> Outdoors </span>
+          <span className="heading-primary--sub"> is where life happens </span>
         </h1>
-        <Button text="Discover our tours" class="btn-white btn-animated" />
+        <Button text="Discover our tours" class="btn--white btn--animated" />
       </div>
     </div>
   );
