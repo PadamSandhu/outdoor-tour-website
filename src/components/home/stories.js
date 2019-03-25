3dc853725757e42e4289afb2565aaedaef236046
Padam <padam@Padams-iMac.local>
2019-03-25 17:50:11 -0400

    added folderstucture

--- src/components/home/stories.js
+++ src/components/home/stories.js
@@ -4,8 +4,10 @@ export default function Stories() {
   return (
     <section className="section-stories">
       <div className="u-center-text u-margin-bottom-big">
-        <h2 className="heading-secondary">Most popular tours</h2>
+        <h2 className="heading-secondary">We make people genuinely happy</h2>
       </div>
+
+      <div className="row">Text</div>
     </section>
   );
 }
